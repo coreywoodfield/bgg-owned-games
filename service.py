1b610f9571f032e7c89a3639adff95bb0fbf4876
Corey Woodfield <coreywoodfield@gmail.com>
2017-08-05 15:48:29 -0600

    Initial commit

---
+++ service.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+import requests
+import csv
+from operator import itemgetter
+
+
+scope = ['https://spreadsheets.google.com/feeds']
+creds = ServiceAccountCredentials.from_json_keyfile_name('client-secret.json', scope)
+client = gspread.authorize(creds)
+
+games_owned = client.open('Games')
+sheet = games_owned.sheet1
+
+people = dict(csv.reader(open('usernames.csv')))
+
+def get_owned_games(username):
+    games = requests.get('https://bgg-json.azurewebsites.net/collection/{}'.format(username)).json()
+    return [game for game in games if game['owned']]
+
+all_games = {}
+for person, username in people.iteritems():
+    games = get_owned_games(username)
+    for game in games:
+         name = game['name']
+         if name in all_games:
+              all_games[name]['owners'].append(person)
+         else:
+              game['owners'] = [person]
+              rank = game['rank']
+              if rank == -1:
+                  game['rank'] = float('inf')
+              all_games[name] = game
+
+
+all_games = sorted(all_games.values(), key=itemgetter('rank'))
+
+sheet.clear()
+sheet.insert_row(['Game', 'Who has it'])
+row = 2
+for game in all_games:
+    sheet.insert_row([game['name'], ', '.join(game['owners'])], row)
+    row += 1
+
